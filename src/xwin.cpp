dcf8521b5c003a3c34b5c7580f86fe2b0a743c6e
Watanabe Iwao <iwao3@DSL.gr.jp>
2013-05-23 14:59:17 +0900

    Xlibで簡易テキスト入力

--- src/xwin.cpp
+++ src/xwin.cpp
@@ -9,6 +9,9 @@
 #include <cstring>
 #include <deque>
 #include <iostream>
+#include <pwd.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <vector>
 
 #include <X11/Xlib.h>
@@ -164,6 +167,7 @@ namespace xwin {
   {
     XDestroyWindow(display, window);
     XCloseDisplay(display);
+    cerr << "#dispose called." << endl;
   }
 
 };
@@ -468,10 +472,381 @@ static int text_list(int argc,char **argv) {
   return 0;
 }
 
+namespace xwin {
+
+  /// テキストを表示/入力するアプリケーション
+  struct xlib03 : xlib02 {
+    XIM input_method;
+    XIC input_context;
+    Atom WM_PROTOCOLS, WM_DELETE_WINDOW;
+    XrmDatabase rmdb;
+    int data_insert_pos;
+    
+    xlib03() : input_method(0),input_context(0),rmdb(0),data_insert_pos(0) { }
+    virtual ~xlib03() {}
+    
+    virtual bool create_input_context(const char *style = "root");
+    virtual void set_window_title(char *title);
+    virtual void insert_text(const wchar_t *wbuf);
+    
+    virtual void set_application_name(char *name, char *class_name);
+    virtual void create_application_window();
+    virtual void event_loop();
+    virtual void dispose();
+  };
+
+  /// ユーザのホームディレクトリを入手する
+  string get_home_dir() {
+    string res;
+    struct passwd *pw;
+    char *ptr;
+
+    ptr = getenv("HOME");
+    if (ptr) {
+      res = ptr;
+      return res;
+    }
+
+    ptr = getenv("USER");
+    pw = ptr ? ::getpwnam(ptr) : ::getpwuid(getuid());
+    if (pw) {
+      res = pw->pw_dir;
+      return res;
+    }
+  
+    return res;
+  }
+
+#ifndef XAPPDEFAULTS_PATH
+#define XAPPDEFAULTS_PATH "/usr/share/X11/app-defaults"
+#endif
+
+  /// アプリケーション名を設定し、入力サーバとの通信を確立する
+  void xlib03::set_application_name(char *res_name, char *res_class) {
+
+    char *modifier = "";
+    if (!XSetLocaleModifiers(modifier))
+      cerr << "WARNING: can not set local modifiders." << endl;
+
+    XrmDatabase rdb, rdbbase;
+    XrmInitialize();
+  
+    const char *appresdir = getenv("XAPPLRESDIR");
+    if (!appresdir) appresdir = getenv("XUSERFILESEARCHPATH");
+    if (!appresdir) appresdir = XAPPDEFAULTS_PATH;
+  
+    rdbbase = XrmGetStringDatabase(appresdir);
+
+    string home_res = get_home_dir();
+    char *server_db = XResourceManagerString(display);
+    if (server_db)
+      rdb = XrmGetStringDatabase(server_db);
+    else {
+      home_res += "/.Xdefaults";
+      rdb = XrmGetFileDatabase(home_res.c_str());
+    }
+    if (rdb) {
+      if (!rdbbase) rdbbase = rdb;
+      else XrmMergeDatabases(rdb,&rdbbase);
+    }
+  
+    home_res = get_home_dir();
+    const char *environment = getenv("XENVIRONMENT");
+    if (environment == NULL) {
+      char hostname[100];
+      gethostname(hostname, sizeof hostname);
+      home_res += "/.Xdefaults-";
+      home_res += hostname;
+      environment = home_res.c_str();
+    }
+    rdb = XrmGetFileDatabase(environment);
+    if (rdb) {
+      if (!rdbbase) rdbbase = rdb;
+      else XrmMergeDatabases(rdb,&rdbbase);
+    }
+
+    rmdb = rdbbase;
+    if (!rmdb) res_name = res_class = NULL;
+
+    input_method = XOpenIM(display, rmdb, res_name, res_class);
+    if (!input_method) {
+      cerr << "WARNING: cann not open input method." << endl;
+      return;
+    }
+  
+    XIMStyles *im_styles;
+
+    XGetIMValues(input_method,
+		 XNQueryInputStyle, &im_styles, NULL);
+  
+    struct { unsigned long style; char *name; }
+    stlist[] = {
+      { XIMPreeditCallbacks, "preedit-callbacks", },
+      { XIMPreeditPosition, "preedit-position", },
+      { XIMPreeditArea, "preedit-area", },
+      { XIMPreeditNothing, "preedit-nothing", },
+      { XIMPreeditNone, "preedit-none", },
+      { XIMStatusCallbacks, "status-callbacks", },
+      { XIMStatusArea, "status-area", },
+      { XIMStatusNothing, "status-nothing", },
+      { XIMStatusNone, "status-none", },
+    };
+
+    cerr << "INFO: supported im style below." << endl;
+
+    string st;
+
+    for (int i = 0; i < im_styles->count_styles; i++) {
+      st = "\t";
+      char *sp = "";
+
+      for (size_t j = 0 ; j < sizeof stlist/sizeof stlist[0]; j++) {
+	if ((im_styles->supported_styles[i] & stlist[j].style) == stlist[j].style) {
+	  st += sp;
+	  st += stlist[j].name;
+	  sp = ", ";
+	}
+      }
+      cerr  << st << endl;      
+    }
+    cerr  << im_styles->count_styles << " support styles found." << endl;
+
+    XFree(im_styles);
+  }
+
+  /// 入力コンテキストの作成
+  bool xlib03::create_input_context(const char *style) {
+    if (!input_method) return false;
+
+    input_context = 
+      XCreateIC( input_method,
+		 XNInputStyle, (XIMPreeditNothing | XIMStatusNothing),
+		 XNClientWindow, window, NULL);
+    /*
+      入力コンテキストは通常、トップ・レベル毎に作成する。
+      作成できないからといって、アプリ全体としては動作しないようにはしない。
+      （従来の英数字の入力はできるため）
+    */
+    
+    if (!input_context) {
+      cerr << "WARNING: cann not create input context.";
+      return false;
+    }
+    return true;
+  }
+
+  /// ウィンドウ・マネージャのタイトルの設定
+  void xlib03::set_window_title(char *title) {
+    XTextProperty prop;
+    int rc = XmbTextListToTextProperty(display,&title,1,XStdICCTextStyle,&prop);
+    if (rc != 0)
+      cerr << "WARNING: XmbTextListToTextProperty failed: " << rc <<endl;
+    else {
+      XSetWMName(display,window,&prop);
+      XFree(prop.value);
+    }
+    cerr << "INFO: window title: " << title <<endl;
+  }
+
+  void xlib03::create_application_window() {
+    xlib02::create_application_window();
+
+    XWMHints hints;
+    hints.input = True;
+    hints.flags = InputHint;
+
+    XSetWMHints(display, window, &hints);
+    /*
+      キーボード入力を必要とすることをウィンドウ・マネージャにに伝える
+    */
+
+    WM_PROTOCOLS = XInternAtom(display, "WM_PROTOCOLS", True );
+    WM_DELETE_WINDOW = XInternAtom(display, "WM_DELETE_WINDOW", True );
+    XSetWMProtocols(display, window, &WM_DELETE_WINDOW, 1 );
+    /*
+      ウィンドウ・マネージャに強制停止ではなく、閉じるのリクエストを送信してもらう
+    */
+    set_window_title("wlib03");
+
+    unsigned long event_mask = 0;
+    if (create_input_context()) {
+      XGetICValues( input_context, XNFilterEvents, &event_mask, NULL );
+      /*
+	インプットメソッドが必要とするイベントマスクを入手する
+      */
+    }
+
+    event_mask |= ButtonPressMask | ExposureMask | KeyPressMask | FocusChangeMask;
+    XSelectInput( display, window, event_mask);
+  }
+
+  /// 入力したテキストをテキストバッファに登録する
+  void xlib03::insert_text(const wchar_t *wbuf) {
+    size_t len = wcslen(wbuf);
+
+    data.replace_text(0, data_insert_pos, wbuf);
+    data_insert_pos += len;
+
+    if (wbuf[len - 1] == L'\n' || wbuf[len - 1] == L'\r') {
+      data.insert_text(0, L"");
+      data_insert_pos = 0;
+    }
+    /*
+     * この実装は文書の先頭に差し込むだけの単純なもの。
+     * 入力内容を確認するだけの振る舞い
+     */
+
+    unsigned width, height;
+    width = height = 1000;
+    XClearArea(display, window, 0, 0, width, height, True);
+
+    wcout << hex;
+    while (*wbuf) {
+      wcout << (int)*wbuf++ << ' ';
+    }
+    wcout << dec << endl;
+  }
+
+  void xlib03::event_loop() {
+    XMapWindow(display, window);
+
+    XEvent event;
+    int exit_flag = 0;
+
+    int wbuf_len = 128;
+    wchar_t *wbuf = (wchar_t *)malloc(wbuf_len * sizeof (*wbuf));
+
+    int buf_len = 64;
+    char *buf = (char *)malloc(buf_len * sizeof (*buf));
+
+    int len;
+    KeySym keysym;
+    Status status;
+    XComposeStatus compose_status;
+
+    while (!exit_flag) {
+      XNextEvent(display, &event);
+      if (XFilterEvent(&event,None)) continue;
+
+      switch (event.type) {
+      case KeyPress: 
+	if (!input_context) {
+	  len = XLookupString(&event.xkey, buf, buf_len, &keysym, &compose_status);
+	  size_t mblen = mbstowcs(wbuf, buf, wbuf_len);
+	  if (mblen == (size_t)-1) {
+	    cerr << "WARNING: invalid state mbstring(ignored)" << endl;
+	    continue;
+	  }
+	  insert_text(wbuf);
+	}
+	else {
+	  len = 
+	    XwcLookupString(input_context,&event.xkey,
+			    wbuf,wbuf_len - 1,&keysym,&status);
+	  wbuf[len] = 0;
+
+	  if (status == XBufferOverflow) {
+	    wchar_t *new_wbuf = (wchar_t *)realloc(wbuf, (len + 1) * sizeof (*wbuf));
+	    if (new_wbuf) {
+	      cerr << "INFO: wbuf expaned to " << (len + 1) << endl;
+	      wbuf_len = len;
+	      wbuf = new_wbuf;
+	      XwcLookupString(input_context,&event.xkey,
+			      wbuf,wbuf_len - 1,&keysym,&status);
+	      wbuf[len] = 0;
+	    }
+	  }
+	  switch(status) {
+	  case XLookupNone: break;
+	  case XLookupKeySym: case XLookupBoth:
+	    if (keysym == XK_Delete) {
+	      cerr << "delete" << endl;
+	    }
+	    else if (keysym == XK_BackSpace) {
+	      cerr << "back space" << endl;
+	    }
+	    else if (keysym == XK_Insert && event.xkey.state & ShiftMask) {
+	      cerr << "try to get selection text" << endl;
+	    }
+	    if (status == XLookupKeySym) break;
+
+	  case XLookupChars: 
+	    insert_text(wbuf);
+	    break;
+	    
+	  default:
+	    cerr << "unkown status" << status << endl;
+	  }
+	}
+      case ButtonPress:
+	break;
+      case Expose:
+	process_expose(&event);
+	break;
+      case FocusIn :
+	if (event.xany.window == window && input_context) 
+	  XSetICFocus(input_context);
+	break;
+      case FocusOut :
+	if (event.xany.window == window && input_context) 
+	  XUnsetICFocus(input_context);
+	break;
+      case ClientMessage:
+	if (event.xclient.message_type == WM_PROTOCOLS &&
+	    (Atom)event.xclient.data.l[0] == WM_DELETE_WINDOW ) {
+	  cerr << "INFO: delete window accepted." << endl;
+	  exit_flag = 1;
+	}
+      }
+    }
+
+    free(wbuf);
+    free(buf);
+  }
+
+  void xlib03::dispose() {
+    if (input_context) XDestroyIC(input_context);
+    if (input_method) XCloseIM(input_method);
+    XrmDestroyDatabase(rmdb);
+    xlib02::dispose();
+  }
+
+};
+
+
+/// テキストを表示するアプリケーション
+static int text_inputs(int argc,char **argv) {
+  xwin::xlib03 *app = new xwin::xlib03;
+
+  const char *text_file = getenv("TEXT");
+  if (!text_file) text_file = __FILE__;
+
+  const char *font_name = getenv("FONT_NAME");
+  if (!font_name) font_name = "-*--14-*,-*--24-*";
+
+  if (!app->locale_initialize()) return 1;
+  if (!app->load_display_text(text_file)) return 1;
+
+  if (!app->connect_server()) return 1;
+  if (!(app->font = app->load_font(font_name))) return 1;
+  
+  char *res_name = basename(argv[0]), *res_class = "Xlib03";
+  app->set_application_name(res_name, res_class);
+
+  app->create_application_window();
+  app->event_loop();
+  app->dispose();
+  return 0;
+}
+
+
 subcmd xwin_cmap[] = {
-  { "win", simple_window, },
-  { "win2", simple_window02, },
-  { "text", text_list, },
+  { "win", simple_window02, },
+  { "win01", simple_window, },
+  { "win02", simple_window02, },
+  { "text", text_inputs, },
+  { "text01", text_list, },
+  { "text02", text_inputs, },
   { 0 },
 };
 
