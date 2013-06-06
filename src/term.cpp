df540cdf2ab7fd5ab10718199a5ae42ec21e0544
Watanabe Iwao <iwao3@DSL.gr.jp>
2013-06-06 10:14:35 +0900

    readline のサンプルコードの追加

--- src/term.cpp
+++ src/term.cpp
@@ -1,14 +1,21 @@
 /*! \file
- * \brief C++の端末制御のサンプルコード
+ * \brief C/C++の端末制御のサンプルコード
  */
 
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
 #include <ncurses.h>
 #include <string>
 #include <sys/ioctl.h>
 #include <vector>
+
+#ifdef USE_READLINE
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
 
 using namespace std;
 
@@ -94,6 +101,8 @@ static int term_show_size(int argc,char **argv) {
   return 0;
 }
 
+// --------------------------------------------------------------------------------
+
 /// curses を使ったサンプルコード
 static int cur_sample01(int argc,char **argv) {
   /*
@@ -139,11 +148,76 @@ static int cur_sample01(int argc,char **argv) {
   return 0;
 }
 
+// --------------------------------------------------------------------------------
+
+#ifdef USE_READLINE
+// readlineライブラリの利用
+
+static int test_GNU_readline(int argc, char **argv) {
+  char *line, *prompt = getenv("PS2");
+  
+  if (prompt == NULL || strcmp("", prompt) == 0)
+	prompt = "readline: ";
+
+  using_history();
+  /*
+	historyライブラリの初期化。
+  */
+
+  string historyfile("config/");
+  
+  historyfile += basename(argv[0]);
+  historyfile += ".history";
+
+  if (read_history(historyfile.c_str()) == 0)
+	cerr << "INFO: " << historyfile << " loaded." << endl;
+  else
+	cerr << "WARNING: read_history failed:" << strerror(errno) << ":" << historyfile << endl;
+
+  while (line = readline(prompt)) {
+	string line_text(line);
+	free(line);
+	/*
+	  返り値はmalloc(3)されたものであるため、free(3)する必要がある。
+	 */
+
+	add_history(line_text.c_str());
+	/*
+	  次の入力候補として登録している。
+	  ここでは省略していうが、実務では記録する履歴件数に上限を設けたほうがよい。
+	 */
+	cerr << line_text << endl;
+	/*
+	  入力したテキストには LF　は含まれていない
+	 */
+	if (line_text == "quit" || line_text == "exit") break;
+  }
+
+  if (write_history(historyfile.c_str()) != 0)
+	cerr << "WARNING: write_history failed:" << strerror(errno) << ":" << historyfile << endl;
+
+  /*
+	historyデータを保存している。
+	ここでは省略しているが、実務では保存する件数を調整したほうがよい。
+   */
+
+  return 0;
+}
+#endif
+
+// --------------------------------------------------------------------------------
+
+#ifdef USE_SUBCMD
+
 #include "subcmd.h"
 
 subcmd term_cmap[] = {
   { "term-size", term_show_size, },
   { "cur", cur_sample01, },
+#ifdef USE_READLINE
+  { "readline", test_GNU_readline, },
+#endif
   { 0 },
 };
 
+#endif
