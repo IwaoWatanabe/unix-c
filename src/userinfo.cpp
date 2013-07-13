f5bbe52042e353ac363a682fc13c72acc737b3cf
Watanabe,Iwao <iwao3@DSL.gr.jp>
2013-07-13 13:06:33 +0900

    グループエントリ取得に対応
    ユーザエントリの不具合修正

--- src/userinfo.cpp
+++ src/userinfo.cpp
@@ -66,7 +66,7 @@ namespace uc {
     /// 保持しているキャッシュ情報を開放する
     virtual void release() = 0;
 
-    static User_Info *get_instance(const char *impl = "");
+    static User_Info *create_instance(const char *impl = "");
   };
 };
 
@@ -83,7 +83,7 @@ namespace {
     map<string, struct passwd *> pwent;
     map<uid_t, struct passwd *> pwent_uid;
     map<string, struct group *> grent;
-    map<uid_t, struct group *> grent_gid;
+    map<gid_t, struct group *> grent_gid;
 
     struct passwd *copy_pwent(struct passwd *pw);
     struct group *copy_grent(struct group *gr);
@@ -113,6 +113,8 @@ namespace {
   };
 
   User_Info_Impl::~User_Info_Impl() { release(); }
+
+  // --------------------------------------------------------------------------------
 
   /// pwent の複製。free 一つで開放できるように配置している
   struct passwd *User_Info_Impl::copy_pwent(struct passwd *pw) {
@@ -149,7 +151,7 @@ namespace {
     *len = 0;
     if (mem) {
       while (*mem) {
-	len += strlen(*mem++) + 1;
+	*len += strlen(*mem++) + 1;
 	ct++;
       }
     }
@@ -163,22 +165,21 @@ namespace {
       p2 = strlen(gr->gr_passwd) + 1;
     size_t p3;
     size_t mct = count_member(gr->gr_mem, &p3);
-
+    len += mct * sizeof ( char * );
     len += p1;
     len += p2;
     len += p3;
-    len += mct * sizeof ( char * );
 
     struct group *ent = (struct group *)malloc(len);
     if (ent) {
       char **pptr = (char **)&ent[1], **mem = gr->gr_mem;
       char *ptr = (char *)(pptr + mct);
+      ent->gr_mem = pptr;
       ent->gr_name = strcpy(ptr, gr->gr_name);
       ent->gr_passwd = strcpy(ptr += p1, gr->gr_passwd);
       ptr += p2;
 
       ent->gr_gid = gr->gr_gid;
-      ent->gr_mem = pptr;
 
       while (*mem) {
 	*pptr = strcpy(ptr, *mem++);
@@ -190,6 +191,8 @@ namespace {
   }
 
 
+  // --------------------------------------------------------------------------------
+
   /// ユーザ名を元にユーザ情報を入手する
   struct passwd *User_Info_Impl::getpwnam(const char *name) {
     map<string, struct passwd *>::iterator it = pwent.find(name);
@@ -198,11 +201,10 @@ namespace {
     struct passwd *ent = ::getpwnam(name);
     if (!ent) return 0;
 
-    ent = copy_pwent(ent);
-    pwent.insert(map<string, struct passwd *>::value_type(name, ent));
-    pwent_uid.insert(map<uid_t, struct passwd *>::value_type(ent->pw_uid, ent));
-
-    return ent;
+    struct passwd *uent = copy_pwent(ent);
+    pwent.insert(map<string, struct passwd *>::value_type(name, uent));
+    pwent_uid.insert(map<uid_t, struct passwd *>::value_type(uent->pw_uid, uent));
+    return uent;
   }
 
   /// ユーザIDを元にユーザ情報を入手する
@@ -213,11 +215,10 @@ namespace {
     struct passwd *ent = ::getpwuid(uid);
     if (!ent) return 0;
 
-    ent = copy_pwent(ent);
-    pwent.insert(map<string, struct passwd *>::value_type(ent->pw_name, ent));
-    pwent_uid.insert(map<uid_t, struct passwd *>::value_type(uid, ent));
-
-    return ent;
+    struct passwd *uent = copy_pwent(ent);
+    pwent.insert(map<string, struct passwd *>::value_type(uent->pw_name, uent));
+    pwent_uid.insert(map<uid_t, struct passwd *>::value_type(uid, uent));
+    return uent;
   }
 
   /// getpwent の呼び出しを開始する
@@ -263,29 +264,71 @@ namespace {
     return pw_count;
   }
 
+  // --------------------------------------------------------------------------------
+
   /// グループ名を元にグループ情報を入手する
   struct group *User_Info_Impl::getgrnam(const char *name) {
-    return 0;
+    map<string, struct group *>::iterator it = grent.find(name);
+    if (it != grent.end()) return it->second;
+
+    struct group *ent = ::getgrnam(name);
+    if (!ent) return 0;
+
+    struct group *gent = copy_grent(ent);
+    grent.insert(map<string, struct group *>::value_type(name, gent));
+    grent_gid.insert(map<gid_t, struct group *>::value_type(gent->gr_gid, gent));
+    return gent;
   }
 
   /// グループIDを元にグループ情報を入手する
   struct group *User_Info_Impl::getgrgid(gid_t gid) {
-    return 0;
+    map<uid_t, struct group *>::iterator it = grent_gid.find(gid);
+    if (it != grent_gid.end()) return it->second;
+
+    struct group *ent = ::getgrgid(gid);
+    if (!ent) return 0;
+
+    struct group *gent = copy_grent(ent);
+    grent.insert(map<string, struct group *>::value_type(gent->gr_name, gent));
+    grent_gid.insert(map<gid_t, struct group *>::value_type(gid, gent));
+    return gent;
   }
 
   /// getgrent の呼び出しを開始する
   void User_Info_Impl::setgrent(bool cache_scan) {
+    ::setgrent();
     gr_count = 0;
   }
 
   /// グループ情報を走査して入手する
   struct group *User_Info_Impl::getgrent(const char *name_prefix) {
+    struct group *gr = ::getgrent();
+    if (!gr) return gr;
+
     gr_count++;
-    return 0;
+
+    map<string, struct group *>::iterator it = grent.find(gr->gr_name);
+    if (it != grent.end()) {
+      if (it->second->gr_gid == gr->gr_gid) return it->second;
+    }
+    else {
+      struct group *ent = copy_grent(gr);
+      grent.insert(map<string, struct group *>::value_type(ent->gr_name, ent));
+      grent_gid.insert(map<gid_t, struct group *>::value_type(ent->gr_gid, ent));
+      return ent;
+    }
+
+    map<gid_t, struct group *>::iterator it2 = grent_gid.find(gr->gr_gid);
+    if (it2 != grent_gid.end()) return it2->second;
+
+    struct group *ent = copy_grent(gr);
+    grent_gid.insert(map<gid_t, struct group *>::value_type(ent->gr_gid, ent));
+    return ent;
   }
 
   /// getgrent の呼び出しを終了する
   int User_Info_Impl::endgrent(void) {
+    ::endgrent();
     return gr_count;
   }
 
@@ -301,6 +344,7 @@ namespace {
 	if (it2 != pwent_uid.end()) {
 	  if (it2->second == it->second) pwent_uid.erase(it2);
 	}
+	free(it->second);
 	pwent.erase(it++);
 	ct++;
       }
@@ -308,18 +352,45 @@ namespace {
 
     {
       map<uid_t, struct passwd *>::iterator it = pwent_uid.begin();
-      while (it != pwent_uid.end()) { pwent_uid.erase(it++); ct++; }
+      while (it != pwent_uid.end()) {
+	free(it->second);
+	pwent_uid.erase(it++);
+	ct++;
+      }
     }
 
-    elog(T, "%d entries released.\n", ct);
+
+    {
+      map<string, struct group *>::iterator it = grent.begin();
+      while (it != grent.end()) {
+	map<gid_t, struct group *>::iterator it2 = grent_gid.find(it->second->gr_gid);
+	if (it2 != grent_gid.end()) {
+	  if (it2->second == it->second) grent_gid.erase(it2);
+	}
+	free(it->second);
+	grent.erase(it++);
+	ct++;
+      }
+    }
+
+    {
+      map<gid_t, struct group *>::iterator it = grent_gid.begin();
+      while (it != grent_gid.end()) {
+	grent_gid.erase(it++);
+	free(it->second);
+	ct++;
+      }
+    }
+
+    elog(T, "%d cache entries released.\n", ct);
   }
 
 };
 
 namespace uc {
 
-  User_Info *User_Info::get_instance(const char *impl) {
-    static User_Info_Impl *ui = new User_Info_Impl();
+  User_Info *User_Info::create_instance(const char *impl) {
+    User_Info_Impl *ui = new User_Info_Impl();
     return ui;
   }
 
@@ -334,7 +405,7 @@ enum { I = uc::ELog::I };
 /// ユーザ・エントリの検索
 static int getpwent01(int argc, char **argv) {
 
-  uc::User_Info *ent = uc::User_Info::get_instance();
+  uc::User_Info *ent = uc::User_Info::create_instance();
   int rc = 0;
 
   optind = 1;
@@ -381,6 +452,58 @@ static int getpwent01(int argc, char **argv) {
   return rc;
 }
 
+
+/// グループ・エントリの検索
+static int getgrent01(int argc, char **argv) {
+
+  uc::User_Info *ent = uc::User_Info::create_instance();
+  int rc = 0;
+
+  optind = 1;
+
+  if (optind == argc) {
+    group *grent;
+    ent->setgrent();
+    while ((grent = ent->getgrent())) {
+      if (putgrent(grent,stdout) != 0) {
+	perror("ERROR: putgrent");
+	rc = 2;
+	break;
+      }
+    }
+
+    elog(I, "%d group entries\n", ent->endgrent());
+
+    delete ent;
+
+    return rc;
+  }
+
+  for (int i = optind; i < argc; i++) {
+    const char *arg = argv[i];
+    group *grent;
+
+    if (!isdigit(*arg))
+      grent = ent->getgrnam(arg);
+    else {
+      gid_t gid = (gid_t)atol(arg);
+      grent = ent->getgrgid(gid);
+    }
+
+    if (!grent) { rc = 1; continue; }
+
+    if (putgrent(grent,stdout) != 0) {
+      perror("ERROR: putgrent");
+      rc = 2;
+      break;
+    }
+  }
+
+  delete ent;
+
+  return rc;
+}
+
 // --------------------------------------------------------------------------------
 
 #ifdef USE_SUBCMD
@@ -388,6 +511,7 @@ static int getpwent01(int argc, char **argv) {
 
 subcmd user_cmap[] = {
   { "getpwent", getpwent01, },
+  { "getgrent", getgrent01, },
   { 0 },
 };
 
