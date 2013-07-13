9d2c85a91cc2e1150f7a5bf108f390a115553927
Watanabe,Iwao <iwao3@DSL.gr.jp>
2013-07-13 22:09:42 +0900

    クラス定義の一部をヘッダフィルに移動

--- src/awt.cpp
+++ src/awt.cpp
@@ -61,106 +61,11 @@ using namespace std;
 extern int getXawListColumns(Widget list);
 extern int getXawListRows(Widget list);
 
-namespace xwin {
-  extern wstring load_wtext(const char *path, size_t buf_len = 4096);
-  extern bool load_dirent(const char *dirpath, vector<string> &entries, bool with_hidden_file = false);
-  extern String *as_String_array(vector<string> &entries);
-};
-
 extern "C" {
   extern char *trim(char *t);
   extern char *demangle(const char *demangle);
 };
 
-
-// --------------------------------------------------------------------------------
-
-/// Xで動作するGUIアプリケーション作成をサポートする
-
-namespace xwin {
-
-  class Client_Context;
-
-  /// 基本メニュー項目を定義する構造体
-  struct Menu_Item {
-    /// メニュー名
-    String name;
-    /// メニュー固有データへのポインタ
-    XtCallbackProc proc;
-    /// メニュー管理主体（通常は Frame）へのポインタ
-    XtPointer closure;
-    /// サブメニュー項目
-    struct Menu_Item *sub_menu;
-     /// メニューの簡易説明テキスト
-    const char *description;
-  };
-
-  /// GUIユーザ・コードが実装するインタフェース
-  struct Frame {
-    virtual ~Frame() = 0;
-    /// コンテナ・リソースを参照するためのコンテキストが渡されてくる
-    virtual void set_Client_Context(Client_Context *context) { }
-    /// アプリケーションのタイトルを返す
-    virtual std::string get_title() { return ""; }
-    /// 実装クラスはこのタイミングでコンテンツを作る
-    virtual Widget create_contents(Widget parent) { return 0; }
-    /// 基本メニュー・アイテムを返す
-    virtual Menu_Item *get_menu_itmes() { return 0; }
-    /// リソースを開放する要求で呼び出される
-    virtual void release() { }
-  };
-
-  /// Frameインスタンスを生成するクラス
-  struct Frame_Factory {
-    Frame_Factory();
-    virtual ~Frame_Factory() { }
-    /// フレームのインスタンスを入手する
-    virtual Frame *get_instance() = 0;
-    /// アプリケーション・クラス名を入手する
-    virtual const char *get_class_name() { return 0; }
-    /// バージョン情報を入手する
-    virtual const char *get_version() { return "0.1"; }
-  };
-
-  /// Xサーバに関連するリソースを保持、管理する
-  /**
-     アトム、フォント、XIM関連を操作することになる
-   */
-  class Server_Resource {
-  protected:
-    /// このリソースを利用するウィジェット
-    Widget owner;
-  public:
-    // 良く利用するアトム
-    Atom WM_PROTOCOLS      /// WMとの通信用
-      ,WM_DELETE_WINDOW  /// トップレベルウインドウの破棄
-      ,COMPOUND_TEXT;   ///< 複合エンコーディングテキスト
-
-    Server_Resource(Widget owner);
-
-    virtual ~Server_Resource() = 0;
-    /// アトム値を入手する
-    virtual Atom atom_value_of(const char *name) = 0;
-    /// アトム・テキストを入手する
-    virtual std::string text_of(Atom atom) = 0;
-    /// 同じサーバを参照するリソースを探す
-    static Server_Resource *find_Server_Resource(Widget shell);
-  };
-
-  /// コンテナのリソースにアクセスするためのインタフェース
-  class Client_Context {
-    virtual ~Client_Context() = 0;
-    /// サーバ情報を入手する
-    virtual Server_Resource *get_Server_Resource() = 0;
-    /// フレームを閉じる操作のためのメニュー項目を返す
-    virtual const Menu_Item *get_close_item() = 0;
-    /// フレームを探す。無ければ作成する
-    virtual Frame *find_Frame(const char *name, const char *frame_class) = 0;
-    /// ダイアログを探す。無ければ作成する
-    virtual Frame *find_Dialog(const char *name, const char *frame_class) = 0;
-  };
-
-};
 
 // --------------------------------------------------------------------------------
 
@@ -180,6 +85,11 @@ namespace {
 // --------------------------------------------------------------------------------
 
 namespace xwin {
+
+  extern wstring load_wtext(const char *path, size_t buf_len = 4096);
+  extern bool load_dirent(const char *dirpath, vector<string> &entries, bool with_hidden_file = false);
+  extern String *as_String_array(vector<string> &entries);
+
 
   Frame::~Frame() { }
 
--- src/xt-proc.h
+++ src/xt-proc.h
@@ -1,5 +1,5 @@
-#ifndef _xt_proc_h
-#define _xt_proc_h
+#ifndef uc_xt_proc_h
+#define uc_xt_proc_h
 
 #include <X11/Intrinsic.h>
 
@@ -20,14 +20,103 @@ extern Boolean dispose_work_proc(XtPointer closure);
 extern Widget find_shell(Widget t, Boolean need_top_level);
 extern void dispose_shell(Widget w);
 
-extern void show_component_tree(Widget target, int level);
-
 #ifdef __cplusplus
 extern void show_component_tree(Widget target, int level = 2);
+#else
+extern void show_component_tree(Widget target, int level);
 #endif
 
 #ifdef __cplusplus
 };
+
+
+/// Xで動作するGUIアプリケーション作成をサポートする
+
+namespace xwin {
+
+  /// 基本メニュー項目を定義する構造体
+  struct Menu_Item {
+    /// メニュー名
+    String name;
+    /// メニュー固有データへのポインタ
+    XtCallbackProc proc;
+    /// メニュー管理主体（通常は Frame）へのポインタ
+    XtPointer closure;
+    /// サブメニュー項目
+    struct Menu_Item *sub_menu;
+     /// メニューの簡易説明テキスト
+    const char *description;
+  };
+
+  struct Client_Context;
+
+  /// GUIユーザ・コードが実装するインタフェース
+  struct Frame {
+    virtual ~Frame() = 0;
+    /// コンテナ・リソースを参照するためのコンテキストが渡されてくる
+    virtual void set_Client_Context(struct Client_Context *context) { }
+    /// アプリケーションのタイトルを返す
+    virtual std::string get_title() { return ""; }
+    /// 実装クラスはこのタイミングでコンテンツを作る
+    virtual Widget create_contents(Widget parent) { return 0; }
+    /// 基本メニュー・アイテムを返す
+    virtual Menu_Item *get_menu_itmes() { return 0; }
+    /// リソースを開放する要求で呼び出される
+    virtual void release() { }
+  };
+
+  /// Frameインスタンスを生成するクラス
+  struct Frame_Factory {
+    Frame_Factory();
+    virtual ~Frame_Factory() { }
+    /// フレームのインスタンスを入手する
+    virtual Frame *get_instance() = 0;
+    /// アプリケーション・クラス名を入手する
+    virtual const char *get_class_name() { return 0; }
+    /// バージョン情報を入手する
+    virtual const char *get_version() { return "0.1"; }
+  };
+
+  /// Xサーバに関連するリソースを保持、管理する
+  /**
+     アトム、フォント、XIM関連を操作することになる
+   */
+  class Server_Resource {
+  protected:
+    /// このリソースを利用するウィジェット
+    Widget owner;
+  public:
+    // 良く利用するアトム
+    Atom WM_PROTOCOLS      /// WMとの通信用
+      ,WM_DELETE_WINDOW  /// トップレベルウインドウの破棄
+      ,COMPOUND_TEXT;   ///< 複合エンコーディングテキスト
+
+    Server_Resource(Widget owner);
+
+    virtual ~Server_Resource() = 0;
+    /// アトム値を入手する
+    virtual Atom atom_value_of(const char *name) = 0;
+    /// アトム・テキストを入手する
+    virtual std::string text_of(Atom atom) = 0;
+    /// 同じサーバを参照するリソースを探す
+    static Server_Resource *find_Server_Resource(Widget shell);
+  };
+
+  /// コンテナのリソースにアクセスするためのインタフェース
+  struct Client_Context {
+    virtual ~Client_Context() = 0;
+    /// サーバ情報を入手する
+    virtual Server_Resource *get_Server_Resource() = 0;
+    /// フレームを閉じる操作のためのメニュー項目を返す
+    virtual const Menu_Item *get_close_item() = 0;
+    /// フレームを探す。無ければ作成する
+    virtual Frame *find_Frame(const char *name, const char *frame_class) = 0;
+    /// ダイアログを探す。無ければ作成する
+    virtual Frame *find_Dialog(const char *name, const char *frame_class) = 0;
+  };
+
+};
+
 #endif
 
 #endif
